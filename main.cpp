362032b34917a54ead4ebaa84f9635954c696bb7
Belouin Nicolas <belouin.nicolas@heptaoctet.net>
2013-12-05 10:25:36 +0100

    Ajout du dossier ressources

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@ int main()
 sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 // Load a sprite to display
 sf::Texture texture;
-if (!texture.loadFromFile("cute_image.jpg"))
+if (!texture.loadFromFile("ressources/cute_image.jpg"))
 return EXIT_FAILURE;
 sf::Sprite sprite(texture);
 // Create a graphical text to display
 sf::Font font;
-if (!font.loadFromFile("arial.ttf"))
+if (!font.loadFromFile("ressources/arial.ttf"))
 return EXIT_FAILURE;
 sf::Text text("Hello SFML", font, 50);
 // Load a music to play
 sf::Music music;
-if (!music.openFromFile("nice_music.ogg"))
+if (!music.openFromFile("ressources/nice_music.ogg"))
 return EXIT_FAILURE;
 // Play the music
 music.play();
