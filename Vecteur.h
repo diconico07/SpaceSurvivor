28f14120bd1c2a12d658e56480f3f79d68e19500
tiritchi <tiritchi@gmail.com>
2013-12-12 11:22:41 +0100

    close issue #1

--- Vecteur.h
+++ Vecteur.h
@@ -24,90 +24,21 @@ private:
 
 public:
 
-	// Constructors/Destructors
-	//  
-
-
-	/**
-	 * Empty Constructor
-	 */
-	/**
-	 * Full constructor
-	 * @param  X
-	 * @param  Y
-	 */
 	Vecteur (int X = 0, int Y = 0 ):x(X),y(Y){}
-
-
-	/**
-	 * Empty Destructor
-	 */
 	virtual ~Vecteur ( );
 
-
-	// Public attribute accessor methods
-	//  
-
-
-
-	 /**
-	  * Set the value of x
-	  * @param new_var the new value of x
-	  */
-	 void setX ( int new_var );
-	 /**
-	  * Get the value of x
-	  * @return the value of x
-	  */
-	 int getX ( );
-
-         /**
-          * Set the value of y
-          * @param new_var the new value of y
-          */
-         void setY ( int new_var );
-
-         /**
-          * Get the value of y
-          * @return the value of y
-          */
-         int getY ( );
-
-         /**
-          * @param  X
-          * @param  Y
-          */
-         void setXY (int X, int Y );
-
-	/**
-	 * @param  module
-	 */
-	void setModule (int module );
-
-
-	/**
-	 * @param  Argument
-	 */
-	void setArgument (float Argument = 0 );
-
-	/**
-	 * @return int
-	 */
-	int getModule ( );
-
-
-	/**
-	 * @return float
-	 */
-	float getArgument ( );
-
-
-	/**
-	 * @param  Module
-	 * @param  Argument
-	 */
+     void setX ( int new_var );
+     int getX ( );
+     void setY ( int new_var );
+     int getY ( );
+     void setXY (int X, int Y );
+    void setModule (int module );
+    void setArgument (float Argument = 0 );
+    int getModule ( );
+    float getArgument ( );
 	void setModuleArgument (int Module, float Argument );
 
+    Vecteur& Vecteur::operator+=(Vecteur vecteur);
 
 };
 
