e7d80cf6c7b9999faa4d7fad67b0b421eb18589d
Belouin Nicolas <belouin.nicolas@heptaoctet.net>
2013-12-19 09:06:10 +0100

    Adds fire delay, avoid the spawning of asteroid on the player, spawns asteroids regularly.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 const int Nombre_asteroids= 6;
 const int Vitesse_projectiles=15;
 const int shootDelay = 100; //100 millisecondes
+const int asteroidDelay = 1000; //1 seconde
 
 int main()
 {
@@ -19,7 +20,9 @@ int main()
 
     Vaisseau *joueur=new Vaisseau();
     listeMissiles.push_back(joueur);
+    bool joueurIsAlive=true;
 
+    sf::Clock fire_delay, asteroid_delay;
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800,600), "Space Survivor");
     // Load a sprite to display
@@ -38,6 +41,8 @@ int main()
     int size;
     for(int i=0;i<Nombre_asteroids;i++){
         position->setXY(std::rand()%750,std::rand()%550);
+        while(position->getX()<joueur->getPosition().getX()+100 && position->getX()>joueur->getPosition().getX()-100 && position->getY()<joueur->getPosition().getY()+100 && position->getY()<joueur->getPosition().getY()-100)
+          position->setXY(std::rand()%750,std::rand()%550);
         vitesse->setXY(std::rand()%6-3,std::rand()%6-3);
         size = std::rand()%5;
         Asteroid *asteroid= new Asteroid(*position,*vitesse,size);
@@ -71,43 +76,63 @@ int main()
                 case sf::Keyboard::Right :
                   joueur->setAccAngulaire(-1);
                   break;
-                case sf::Keyboard::Space : { //Creates a new scope in order to allow variable declaration in the switch case
+                case sf::Keyboard::Space : if(fire_delay.getElapsedTime().asMilliseconds()>shootDelay && joueurIsAlive){ //Creates a new scope in order to allow variable declaration in the switch case
                     Projectile *projectile = new Projectile(joueur->getPosition()+joueur->getVitesse()+Vecteur(joueur->getSize(),(float)joueur->getAngle()),Vecteur(Vitesse_projectiles,(float)joueur->getAngle()),joueur->getAngle());
                   listeMissiles.push_back(projectile);
-                  break;
+                  fire_delay.restart();
                   }
+                  break;
                 case sf::Keyboard::Escape :
                   window.close();
                 default:
                   break;
                 }
         }
+
+
         // Clear screen
         window.clear();
-        // Draw the sprites
+
+        // Move the sprites
+        if(joueurIsAlive){
+            if(asteroid_delay.getElapsedTime().asMilliseconds()>asteroidDelay){
+                asteroid_delay.restart();
+                position->setXY(std::rand()%750,std::rand()%550);
+                while(position->getX()<joueur->getPosition().getX()+100 && position->getX()>joueur->getPosition().getX()-100 && position->getY()<joueur->getPosition().getY()+100 && position->getY()<joueur->getPosition().getY()-100)
+                  position->setXY(std::rand()%750,std::rand()%550);
+                vitesse->setXY(std::rand()%6-3,std::rand()%6-3);
+                size = std::rand()%5;
+                Asteroid *asteroid= new Asteroid(*position,*vitesse,size);
+                listeMissiles.push_back(asteroid);
+              }
+
+            for(unsigned int i=0;i<listeMissiles.size();i++){
+                try{
+                  listeMissiles[i]->move();
+                }
+                catch(int e) {
+                  if(e==10){
+                      delete listeMissiles[i];
+                      listeMissiles.erase(listeMissiles.begin()+i);
+                    }
+                }
+
+              }
+            for(unsigned int i=0;i<listeMissiles.size();i++){
+                for(unsigned int j=i+1;j<listeMissiles.size();j++)
+                  if(listeMissiles[i]->collide(listeMissiles[j])){
+                      if(listeMissiles[i]==joueur || listeMissiles[j]==joueur)
+                        joueurIsAlive=false;
+                      delete listeMissiles[j];
+                      delete listeMissiles[i];
+                      listeMissiles.erase(listeMissiles.begin()+j);
+                      listeMissiles.erase(listeMissiles.begin()+i);
+                    }
+
+              }
+          }
+        //Draw the sprites
         window.draw(back);
-        for(unsigned int i=0;i<listeMissiles.size();i++){
-            try{
-            listeMissiles[i]->move();
-            }
-            catch(int e) {
-              if(e==10){
-                  delete listeMissiles[i];
-                  listeMissiles.erase(listeMissiles.begin()+i);
-                }
-            }
-
-        }
-        for(unsigned int i=0;i<listeMissiles.size();i++){
-            for(unsigned int j=i+1;j<listeMissiles.size();j++)
-              if(listeMissiles[i]->collide(listeMissiles[j])){
-                  delete listeMissiles[j];
-                  delete listeMissiles[i];
-                  listeMissiles.erase(listeMissiles.begin()+j);
-                  listeMissiles.erase(listeMissiles.begin()+i);
-                }
-
-        }
         for(unsigned int i=0;i<listeMissiles.size();i++){
             for(int j=0;j<4;j++)
               window.draw(listeMissiles[i]->getSprite(j));
